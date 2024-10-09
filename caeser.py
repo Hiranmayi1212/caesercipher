a4a2aef38e4aa811f0f1319f7d398c1cfdacef2c
Hiranmayi1212 <madiraju.hiranmayi@gmail.com>
2024-10-09 22:58:23 +0530

    a test branch with reduced number of lines

--- caeser.py
+++ caeser.py
@@ -7,33 +7,27 @@ ws = WritingSystem()
 a=ws.by_language(ws.Language.English, letter_case=ws.LetterCase.Lower, as_list=True)
 
 
-def encrypt(c,s):
-    for i in s:
-        if i.lower() in a:
-            n=a.index(i.lower())
-            n=n+c
-            b=a[(n%25)]
-            if i.isupper()==True:
-                b=b.upper()
-            s=s.replace(i,b)
-        
-    print(s) 
+def encrypt(n,c):
+    n+c
+    return n
     
-def decrypt(c,s):
-    for i in s:
-        if i.lower() in a:
-            n=a.index(i.lower())
-            n=n-c
-            b=a[(n%25)]
-            if i.isupper()==True:
-                b=b.upper()
-            s=s.replace(i,b)
-    print(s) 
+def decrypt(n,c):
+    n-c
+    return n
 
 c=int(input("enter the cipher value"))
 s=input("enter string ")
 d=int(input("enter 1 for encrypt 2 for decrypt"))
-if d==1:
-    encrypt(c,s)
-elif d==2:
-    decrypt(c,s)
+for i in s:
+        if i.lower() in a:
+            n=a.index(i.lower())
+            if d==1:
+                encrypt(n,c)
+            elif d==2:
+                decrypt(n,c)
+            b=a[(n%25)]
+            if i.isupper()==True:
+                b=b.upper()
+            s=s.replace(i,b)
+
+print(s) 
